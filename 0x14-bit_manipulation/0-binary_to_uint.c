0d8c569d3676dc1071cce4ce812f617f7312c80a
adisu-mulu <kulhabesh31@gmail.com>
2024-02-09 09:20:59 +0300

    0x14-bit-manipulation

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include "main.h"
+
+unsigned int binary_to_uint(const char *b)
+{
+    int count = 0;
+    int sum = 0;
+    int multiplier = 0;
+    int product;
+    while (b[count] != '\0')
+    {
+        count++;
+    }
+    for (int i = count - 1; i >= 0; i--)
+    {
+        if (multiplier == 0)
+        {
+            product = 1;
+        }
+        else
+        {
+            product=1;
+            for (int mult = multiplier; mult > 0; mult--)
+            {
+                product = product * 2;
+            }
+        }
+        int to_digit = b[i] - '0';
+        if (to_digit != 0 & to_digit != 1)
+        {
+            sum = 0;
+            return 0;
+        }
+        else
+        {
+            to_digit = to_digit * product;
+            sum = sum + to_digit;
+            multiplier++;
+        }
+    }
+
+    return sum;
+}
