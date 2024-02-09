c252538f8c01694bb96a7fc09a6bf7c95db0fd16
adisu-mulu <kulhabesh31@gmail.com>
2024-02-09 22:22:26 +0300

    adding 2-getbit

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#define SIZE 100
+/**
+ * get_bit - returns the bit on the specified index
+ * @index: specifies the index to be returned
+ * @n: the integer that is going to be converted to bits
+ * Return: the bit at the specified index
+ */
+
+
+int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned int i;
+	int bits[SIZE];
+	int bit;
+
+	for (i = 0; i <= index; i++)
+	{
+		bits[i] = n % 2;
+		n = n / 2;
+	}
+	bit = bits[index];
+	return (bit);
+}
