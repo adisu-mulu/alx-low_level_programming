b9d07c76b918e7a762f810f53f968ac0bab0d12a
adisu-mulu <kulhabesh31@gmail.com>
2024-02-10 10:24:47 +0300

    updating 1-print.

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 #include "main.h"
 /**
  * print_binary - converts the given number
@@ -6,188 +5,34 @@
  */
 void print_binary(unsigned long int n)
 {
-	unsigned long int c = 524288;
-	unsigned long int d = 262144;
-	unsigned long int e = 131072;
-	unsigned long int f = 65536;
-	unsigned long int g = 32768;
-	unsigned long int h = 16384;
-	unsigned long int i = 8192;
-	unsigned long int j = 4096;
-	unsigned long int k = 2048;
-	unsigned long int l = 1024;
-	unsigned long int m = 512;
-	unsigned long int ne = 256;
-	unsigned long int o = 128;
-	unsigned long int p = 64;
-	unsigned long int q = 32;
-	unsigned long int r = 16;
-	unsigned long int s = 8;
-	unsigned long int t = 4;
-	unsigned long int u = 2;
-	unsigned long int v = 1;
-	char cc = '0', dd = '0 ', ee = '0', ff = '0', gg = '0', hh = '0', ii = '0', jj = '0', kk = '0', ll = '0', mm = '0', nene = '0', oo = '0', pp = '0', qq = '0 ', rr = '0', ss = '0', tt = '0', uu = '0', vv = '0';
-	int c_set = 0;
-	int d_set = 0;
-	int e_set = 0;
-	int f_set = 0;
-	int g_set = 0;
-	int h_set = 0;
-	int i_set = 0;
-	int j_set = 0;
-	int k_set = 0;
-	int l_set = 0;
-	int m_set = 0;
-	int n_set = 0;
-	int o_set = 0;
-	int p_set = 0;
-	int q_set = 0;
-	int r_set = 0;
-	int s_set = 0;
-	int t_set = 0;
-	int u_set = 0;
-	int v_set = 0;
+	int i;
+	int bit;
+	unsigned long int decimal = n;
+	int required_bits = 0;
 
-	
-	if (n >= c)
+	if (n == 0)
+		required_bits = 1;
+	else if (n == 1)
+		required_bits = 1;
+	else
 	{
-		n = n - c;
-		c_set = 1;
-		cc = c_set + '0';
+		while (n > 0)
+		{	
+			n = n >> 1;
+			required_bits++;
+		}
 	}
-	if (n >= d) 
+	for (i = required_bits - 1; i >= 0; i--)
 	{
-		n = n - d;
-		d_set = 1;
-		dd = d_set + '0';
+		bit = decimal >> i;
+		if (bit & 1)
+		{
+			_putchar('1');
+		}
+		else
+		{
+			_putchar('0');
+		}
 	}
-	if (n >= e)
-	{
-		n = n - e;
-		e_set = 1;
-		ee = e_set + '0';
-	}
-	if (n >= f)
-	{
-		n = n - f;
-		f_set = 1;
-		ff = f_set + '0';
-	}
-	if (n >= g)
-	{
-		n = n - g;
-		g_set = 1;
-		gg = g_set + '0';
-	}
-	if (n >= h)
-	{
-		n = n - h;
-		h_set = 1;
-		hh = h_set + '0';
-	}
-	if (n >= i) 
-	{
-		n = n - i;
-		i_set = 1;
-		ii = i_set + '0';
-	}
-	if (n >= j)
-	{
-		n = n - j;
-		j_set = 1;
-		jj = j_set + '0';
-	}
-	if (n >= k) 
-	{
-		n = n - k;
-		k_set = 1;
-		kk = k_set + '0';
-	}
-	if (n >= l)
-	{
-		n = n - l;
-		l_set = 1;
-		ll = l_set + '0';
-	}
-	if (n >= m)
-	{
-		n = n - m;
-		m_set = 1;
-		mm = m_set + '0';
-	}
-	if (n >= ne)
-	{
-		n = n - ne;
-		n_set = 1;
-		nene = n_set + '0';
-	}
-	if (n >= o)
-	{
-		n = n - o;
-		o_set = 1;
-		oo = o_set + '0';
-	}
-	if (n >= p)
-	{
-		n = n - p;
-		p_set = 1;
-		pp = p_set + '0';
-	}
-	if (n >= q) 
-	{
-		n = n - q;
-		q_set = 1;
-		qq = q_set + '0';
-	}
-	if (n >= r)
-	{
-		n = n - r;
-		r_set = 1;
-		rr = r_set + '0';
-	}
-	if (n >= s) 
-	{
-		n = n - s;
-		s_set = 1;
-		ss = s_set + '0';
-	}
-	if (n >= t)
-	{
-		n = n - t;
-		t_set = 1;
-		tt = t_set + '0';
-	}
-	if (n >= u)
-	{
-		n = n - u;
-		u_set = 1;
-		uu = u_set + '0';
-	}
-	if (n >= v) 
-	{
-		v_set = 1;
-		vv = v_set + '0';
-	}
-	_putchar(cc);
-	_putchar(dd);
-	_putchar(ee);
-	_putchar(ff);
-	_putchar(gg);
-	_putchar(hh);
-	_putchar(ii);
-	_putchar(jj);
-	_putchar(kk);
-	_putchar(ll);
-	_putchar(mm);
-	_putchar(nene);
-	_putchar(oo);
-	_putchar(pp);
-	_putchar(qq);
-	_putchar(rr);
-	_putchar(ss);
-	_putchar(tt);
-	_putchar(uu);
-	_putchar(vv);
 }
-
 
