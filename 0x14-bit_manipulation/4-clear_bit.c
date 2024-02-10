bad3e7f5d1a317fbb6ecb9384e711cac58dfa34f
adisu-mulu <kulhabesh31@gmail.com>
2024-02-10 22:38:51 +0300

    adding 4-clear

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * clear_bit - function used to set the bit to 0
+ * @n: the decimal whose bit is to be set 0
+ * @index: the index to be changed
+ */
+
+
+
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned long mask;
+	unsigned long int l = sizeof(unsigned long int) * 8;
+
+	if (index >= 1)
+	{
+		return (-1);
+	}
+	mask = 0UL << index;
+	*n = *n | mask;
+	return (1);
+}
