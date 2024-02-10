b2c49e4cd36a583ff75918a94b796fb58d1ed5e3
adisu-mulu <kulhabesh31@gmail.com>
2024-02-10 22:49:08 +0300

    updating 4clear

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -13,11 +13,11 @@ int clear_bit(unsigned long int *n, unsigned int index)
 	unsigned long mask;
 	unsigned long int l = sizeof(unsigned long int) * 8;
 
-	if (index >= 1)
+	if (index >= l)
 	{
 		return (-1);
 	}
-	mask = ~(1UL << indexi;
+	mask = ~(1UL << index);
 	*n = *n & mask;
 	return (1);
 }
