fc2d3522623a301cfa102de4e15fbc164a7370e7
adisu-mulu <kulhabesh31@gmail.com>
2024-02-12 23:44:39 +0300

    modifying 2-append

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -12,8 +12,8 @@ int append_text_to_file(const char *filename, char *text_content)
 	int fd;
 	int letters;
 	int rwr;
-	
-	if(!filename)
+
+	if (!filename)
 		return (-1);
 	fd = open(filename, O_WRONLY | O_QPPEND);
 	if (fd == -1)
