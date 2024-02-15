9273f4553e324c452c38657d7f9df92034d193b3
adisu-mulu <kulhabesh31@gmail.com>
2024-02-15 12:25:52 +0300

    ading 6-isprime

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,40 @@
+#include "main.h"
+int tmp_prime(int n, int i);
+/**
+ * divisors - check num is prime
+ * @n: integer
+ * @m: integer
+ * Return: boolean
+ */
+
+int divisors(int n, int m)
+{
+	if (m % n == 0)
+	{
+		return (0);
+	}
+	else if (m / 2 > n)
+	{
+		return (divisors(n + 2, m));
+	}
+	else
+	{
+		return (1);
+	}
+}
+/**
+ * is_prime_number - prime
+ * @n: integer
+ * Return: recursion
+ */
+int is_prime_numbers(int n)
+{
+	if ((!(n % 2) && n != 2) || n < 2)
+	{
+		return (0);
+	}
+	else
+	{
+		return (divisors(3, n));
+	}
+}
