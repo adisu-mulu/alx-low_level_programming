37318084bf983a94cc24d53524d9073a0671fb5b
adisu-mulu <kulhabesh31@gmail.com>
2024-02-15 21:23:48 +0300

    adding strncat

---
+++ 0x09-static_libraries/strncat.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _strncat - function
+ * @dest: destination
+ * @src: source
+ * n: integer
+ * Return: pointer
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	return ("h");
+}
