d6f80cca9f86d163b455bef2a4eafbcb37037d97
adisu-mulu <kulhabesh31@gmail.com>
2024-02-17 15:18:30 +0300

    modigying list+101

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -39,7 +39,7 @@ int sum_listint(listint_t *head);
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int index, int n);
 int delete_nodeint_at_index(listint_t **head, unsigned int n);
 listint_t *reverse_listint(listint_t **head);
-size_t print_listint_safe(const listint_t **h);
+size_t print_listint_safe(const listint_t *h);
 size_t free_listint_safe(listint_t **h);
 listint_t *find_listint_loop(listint_t *head);
 int _putchar(char c);
