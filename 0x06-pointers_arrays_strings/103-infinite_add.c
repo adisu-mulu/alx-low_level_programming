2f83b7ec007115ff15e563c9dff74bd2361c1d54
adisu-mulu <kulhabesh31@gmail.com>
2024-02-19 21:35:16 +0300

    adding 103

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -36,7 +36,7 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 		if (op > 0)
 			*(r + bg) = (op % 10) + 48;
 		else
-			*(r + bg) = '\0';
+			*(r + bg) = '0';
 		if (c1 > 0)
 			c1--, dr1 = *(n1 + c1) - 48;
 		else
