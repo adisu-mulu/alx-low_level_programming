94c3eee4ff7a95c34da6ca9b650d9cb495078587
adisu-mulu <kulhabesh31@gmail.com>
2024-03-21 16:18:44 +0300

    updating 0-p

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,13 +1,21 @@
 #include<stdio.h>
 #include "lists.h"
 
+/**
+ * print_dlistint - function
+ * @h: double pointer
+ * Return: counter
+ */
+
 size_t print_dlistint(const dlistint_t *h)
 {
 	const dlistint_t *temp = h;
 	int count = 0;
 
 	if (temp == NULL)
+	{
 		return (0);
+	}
 	else
 	{
 		while (temp != NULL)
