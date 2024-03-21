c341264df18a380d68cfe91f63cf790d6637f642
adisu-mulu <kulhabesh31@gmail.com>
2024-03-21 17:10:01 +0300

    adding 2-add

---
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,36 @@
+#include "lists.h"
+/**
+ * add_dnodeint - function
+ * @head: double pointer
+ * @n: data
+ * Return: address
+ */
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *newnode;
+
+	newnode = (dlistint_t *)(malloc(sizeof(dlistint_t)));
+	if (!newnode)
+	{
+		return (NULL);
+	}
+	else
+	{
+		if (*head == NULL)
+		{
+			*head = newnode;
+			newnode->n = n;
+			newnode->next = NULL;
+			newnode->prev = NULL;
+		}
+		else
+		{
+			newnode->n = n;
+			newnode->next = *head;
+			(*head)->prev = newnode;
+			newnode->prev = NULL;
+			*head = newnode;
+		}
+	}
+	return (newnode);
+}
